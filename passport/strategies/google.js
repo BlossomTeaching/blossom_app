bf456f6379c9cb018fa9aeb009f9847e5a952611
mario010203 <mariojose1490@hotmail.com>
2020-02-19 21:06:27 +0100

    google added

--- passport/strategies/google.js
+++ passport/strategies/google.js
@@ -1,15 +1,18 @@
+require("dotenv").config();
 const passport = require("passport");
-const GoogleStrategy = require("passport-google").Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const clientID = process.env.clientID;
+const clientSecret = process.env.clientSecret;
 
 passport.use(
   new GoogleStrategy(
     {
-      returnURL: "http://localhost:3000/auth/google/return",
-      realm: "http://localhost:3000/"
+      clientID,
+      clientSecret,
+      callbackURL: "http://localhost:3000/auth/google/callback"
     },
-
-    function(identifier, done) {
-      User.findByOpenID({ openId: identifier }, function(err, user) {
+    function(accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function(err, user) {
         return done(err, user);
       });
     }
