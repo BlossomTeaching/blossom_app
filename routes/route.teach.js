72f974b1a3aa28796584a230c10c5badfcf5ad5b
mario010203 <mariojose1490@hotmail.com>
2020-02-27 19:38:04 +0100

    link from teacher to student done

--- routes/route.teach.js
+++ routes/route.teach.js
@@ -5,11 +5,21 @@ const User = require("../models/User");
 router.get("/", async (req, res, next) => {
   try {
     const students = await User.find({ roll: "Student" });
-    res.render("teacherpanel", { students });
+    res.render("teach/teacherpanel", { students });
   } catch (e) {
     next();
   }
   console.log(students);
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const students = await User.findById(id);
+    res.render("learn/statistics", { students });
+  } catch (e) {
+    next();
+  }
 });
 
 /* router.get("/",  async (req, res) => {
