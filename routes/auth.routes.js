4377b3daf1ddcb42c7f853bd08c4ecc23c1590a9
mario010203 <mariojose1490@hotmail.com>
2020-02-29 12:57:43 +0100

    flash working

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,7 @@ const { isLoggedOut, isLoggedIn } = require("../lib/isLogged");
 const { hashPassword } = require("../lib/hashing");
 
 router.get("/signup", isLoggedOut(), (req, res) => {
-  res.render("auth/signup"), { signup: true };
+  res.render("auth/signup");
 });
 
 router.post("/signup", isLoggedOut(), async (req, res) => {
@@ -21,35 +21,26 @@ router.post("/signup", isLoggedOut(), async (req, res) => {
       teacheremail,
       password: hashPassword(password)
     });
-    req.login(newUser, () => {
-      return res.redirect("/");
-    });
+
+    return res.redirect("/");
   } else {
-    req.flash("error", "User already exists");
-    return res.redirect("/auth/signup");
+    req.flash("error", "Username already exits");
+    return res.render("auth/signup", { messages: req.flash("error") });
   }
 });
 
 router.get("/login", isLoggedOut(), (req, res) => {
-  res.render("auth/login", { login: true });
+  res.render("auth/login");
 });
 
-router.post("/login", isLoggedOut(), (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.redirect("/auth/login");
-    }
-    req.logIn(user, err => {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect("/");
-    });
-  })(req, res, next);
-});
+router.post(
+  "/login",
+  isLoggedOut(),
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login"
+  })
+);
 
 router.get("/logout", isLoggedIn(), (req, res, next) => {
   req.logout();
